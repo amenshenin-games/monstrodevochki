81ddc944efaf53026e0a5f69c481b837bdf100cc
WShrimp <164909559+WShrimp@users.noreply.github.com>
2026-08-29 16:52:20 +0300

    SignalBus
    Added SignalBus (as autolod) and simplified CommandHandler. + temporary VisualSystem and test yarn file

--- Scripts/Systems/CommandHandler.cs
+++ Scripts/Systems/CommandHandler.cs
@@ -1,10 +1,37 @@
 /*
-Handles commands inside yarn files
-USAGE: 
-<<change_portrait NAME>> - changes character icon on the bottom left
-<<change_image NAME>> - changes character image
-<<change_background NAME>>
+	КОМАНДЫ.
+	(везде, где есть 'name', можно вставить 'none', чтобы очистить)
+	
+	0. Ресет
+	reset() - убирает изображения персонажей, убирает фон, убирает музыку.
+
+	1. Изображения персонажей (или предмета. короче, картинка)
+	add_image('name') - добавляет изображение в текст.
+	change_image('name') - убирает все изображения, добавляет новое.
+
+	2. Бэкграунд - независим от персонажа, находится под ним.
+	change_background('name') - устанавливает фон.
+
+	3. Воспроизведение звука
+	play_sound('name') - воспроизводит единичный звук, независим от музыки
+	play_music('name') - устанавливает фоновую музыку
+
+	4. Прокачка
+	upgrade_stat('stat') - производит улучшение характиристики
+	get_stat('stat') - возвращает значение характеристики
+	set_stat('stat', value) - устанавливает значениие характеристики (скорее для отладки)
+
+	5. Добавляет в систему диалога пункт выбора.
+	check('check_id', difficulty, 'stat', disadvantage, advantage)
+	пример: <<check('door_lock', 10, 'strength', $arm_broken, false)>>
+	
+
+	6. пассивная проверка, возвращает true или false
+	passive_check('check_id', difficulty, 'stat', disadvantage, advantage)
+
+	Потенциально: добавить персонажа в список доступных для добавления в отряд, добавить в отряд и т.д. связанные с отрядом.
 */
+
 
 using Godot;
 using System;
@@ -12,98 +39,87 @@ using YarnSpinnerGodot;
 
 public partial class CommandHandler : Node
 {
-	static TextureRect characterPortrait;
-	static TextureRect characterImage;
-	static TextureRect background;
+
+	static Node signalBus;
+	// private InMemoryVariableStorage variableStorage;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		characterPortrait = GetNode<TextureRect>("../LinePresenter/PresenterControl/HBoxContainer/CharacterPortrait");
-		characterImage = GetNode<TextureRect>("../CharacterImage");
-		background = GetNode<TextureRect>("../Background");
+		signalBus = GetNode<Node>("/root/SignalBus");
+		// variableStorage = GetNode<InMemoryVariableStorage>("../InMemoryVariableStorage");
+	}	
+
+
+// reset
+	[YarnCommand("reset")]
+	public static void Reset(string Name)
+	{
+		signalBus.EmitSignal("reset", Name);
 	}
 
-	[YarnCommand("change_portrait")]
-	public static void ChangePortrait(string portraitName)
+// visual
+	[YarnCommand("add_image")]
+	public static void AddImage(string Name)
 	{
-		// GD.Print("[CommandHandler] Change portrait to ", portraitName);
-		if (characterPortrait != null) 
-		{
-			var characterPortraitTexture = GD.Load<Texture2D>("res://Assets/Art/Portraits/" + portraitName + ".png");
-			if (characterPortraitTexture != null) 
-			{
-				characterPortrait.Texture = characterPortraitTexture;
-			}
-			else
-			{
-				GD.Print("[CommandHandler] Portrait texture not found");
-			}
-		}
-		else
-		{
-			GD.Print("[CommandHandler] node CharacterPortrait not found");
-		}
+		signalBus.EmitSignal("add_image", Name);
 	}
-	
-	[YarnCommand("change_character")]
-	public static void ChangeCharacter(string characterName)
+
+	[YarnCommand("change_image")]
+	public static void ChangeImage(string Name)
 	{
-		// GD.Print("[CommandHandler] Change character to ", characterName);
-		if (characterImage != null)
-		{
-			if (characterName == "0" || characterName == "none")
-			{
-				characterImage.Visible = false;
-			}
-			else
-			{
-				characterImage.Visible = true;
-				var characterTexture = GD.Load<Texture2D>("res://Assets/Art/Characters/" + characterName + ".png");
-				if (characterTexture != null)
-				{
-					characterImage.Texture = characterTexture;
-				}
-				else
-				{
-					GD.Print("[CommandHandler] Character texture not found");
-					return;
-				}
-			}
-		}
-		else
-		{
-			GD.Print("[CommandHandler] node CharacterImage not found");
-		}
+		signalBus.EmitSignal("change_image", Name);
 	}
 
 	[YarnCommand("change_background")]
-	public static void ChangeBackground(string backgroundName)
+	public static void ChangeBackground(string Name)
 	{
-		// GD.Print("[CommandHandler] Change background to ", backgroundName);
-		if (background != null)
-		{
-			var backgroundTexture = GD.Load<Texture2D>("res://Assets/Art/Backgrounds/" + backgroundName + ".png");
-			if (backgroundTexture != null)
-			{
-				background.Texture = backgroundTexture;
-			}
-			else
-			{
-				backgroundTexture = GD.Load<Texture2D>("res://Assets/Art/Backgrounds/" + backgroundName + ".jpg");
-				if (backgroundTexture != null)
-					{
-						background.Texture = backgroundTexture;
-					}
-				else
-				{
-					GD.Print("[CommandHandler] Background texture not found");
-				}
-			}
-		}
-		else
-		{
-			GD.Print("[CommandHandler] node Background not found");
-		}
+		signalBus.EmitSignal("change_background", Name);
 	}
+
+// sound
+	[YarnCommand("play_sound")]
+	public static void PlaySound(string Name)
+	{
+		signalBus.EmitSignal("play_sound", Name);
+	}
+
+	[YarnCommand("play_music")]
+	public static void PlayMusic(string Name)
+	{
+		signalBus.EmitSignal("play_music", Name);
+	}
+
+// stats
+	[YarnCommand("upgrade_stat")]
+	public static void UpgradeStat(string Stat)
+	{
+		signalBus.EmitSignal("upgrade_stat", Stat);
+	}
+
+	[YarnCommand("get_stat")]
+	public static void GetStat(string Stat)
+	{
+		signalBus.EmitSignal("get_stat", Stat);
+	}
+
+	[YarnCommand("set_stat")]
+	public static void SetStat(string Stat, int Value)
+	{
+		signalBus.EmitSignal("set_stat", Stat, Value);
+	}
+
+// check
+	[YarnCommand("check")]
+	public static void Check(string CheckId, int Difficulty, string Stat, bool Disadvantage, bool Advantage)
+	{
+		signalBus.EmitSignal("check", CheckId, Difficulty, Stat, Disadvantage, Advantage);
+	}
+
+	[YarnCommand("passive_check")]
+	public static void PassiveCheck(string CheckId, int Difficulty, string Stat, bool Disadvantage, bool Advantage)
+	{
+		signalBus.EmitSignal("passive_check", CheckId, Difficulty, Stat, Disadvantage, Advantage);
+	}
+
 }
